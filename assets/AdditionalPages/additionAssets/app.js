bdac067adf0c6272d8a6e69d59125eb345986215
Bharath Ravelli <bharathravelli419@gmail.com>
2023-07-02 22:56:17 +0530

    Completed Website

--- assets/AdditionalPages/additionAssets/app.js
+++ assets/AdditionalPages/additionAssets/app.js
@@ -1,7 +1,7 @@
-const pdfLink = document.querySelector('.pdf-link');
+const pdfLink = document.querySelector('.links-attached');
 pdfLink.addEventListener('click', (event) => {
   event.preventDefault();
-  const pdfUrl = "https://drive.google.com/file/d/1lo9KfP_KRMI8htlpMVF2WXfxuMmxu06K/view?usp=sharing";
+  const pdfUrl = pdfLink.getAttribute('href');
   const embedHtml = `<embed src="${pdfUrl}" type="application/pdf" width="100%" height="500px" />`;
   document.body.innerHTML = embedHtml;
 });
